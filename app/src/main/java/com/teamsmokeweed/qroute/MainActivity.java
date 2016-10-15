40f887f1bcb2c2561701067488df2dfb1b7bf695
suchaj jongprasit <arm19jong@gmail.com>
2016-10-15 16:50:03 +0700

    Add GenQr

--- app/src/main/java/com/teamsmokeweed/qroute/MainActivity.java
+++ app/src/main/java/com/teamsmokeweed/qroute/MainActivity.java
@@ -1,13 +1,29 @@
 package com.teamsmokeweed.qroute;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.teamsmokeweed.qroute.genqr.GenQrActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonToGen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonToGen = (Button) findViewById(R.id.ButtonToGen);
+        buttonToGen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), GenQrActivity.class);
+                //i.putExtra("sendTextStr",sendText.getText().toString() );
+                startActivity(i);
+            }
+        });
     }
 }
