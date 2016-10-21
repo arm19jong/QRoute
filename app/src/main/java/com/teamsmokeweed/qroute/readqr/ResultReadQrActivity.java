7e5361f54f9100cd6caedff22fd3225769adeac7
suchaj jongprasit <arm19jong@gmail.com>
2016-10-21 10:58:31 +0700

    chande Ui result ger qr

--- app/src/main/java/com/teamsmokeweed/qroute/readqr/ResultReadQrActivity.java
+++ app/src/main/java/com/teamsmokeweed/qroute/readqr/ResultReadQrActivity.java
@@ -3,6 +3,7 @@ package com.teamsmokeweed.qroute.readqr;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Point;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Display;
@@ -10,6 +11,7 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -38,10 +40,13 @@ import java.util.List;
 public class ResultReadQrActivity  extends AppCompatActivity {
 
 
-    private Button route, reGenQr;
-    private TextView titles, placeName, placeType, des, latLng, webPage;
+//    private Button route;
+    private TextView reGenQr, des, webPage;
     String[] sQr;
     DateQr dateQr;
+    ImageButton imgRoute;
+
+    TextView placeTypeBar, placeNameBar;
 
 
     @Override
@@ -49,15 +54,15 @@ public class ResultReadQrActivity  extends AppCompatActivity {
         super.onCreate(state);
         setContentView(R.layout.result_read_qr);
 
-        titles = (TextView) findViewById(R.id.titles_ans);
-        placeName = (TextView) findViewById(R.id.placeName_ans);
-        placeType = (TextView) findViewById(R.id.placeType_ans);
-        des = (TextView) findViewById(R.id.des_ans);
-        latLng = (TextView) findViewById(R.id.latLng_ans);
-        webPage = (TextView) findViewById(R.id.webPage_ans);
-        route = (Button) findViewById(R.id.route);
-        reGenQr = (Button) findViewById(R.id.reGenQr);
+//        route = (Button) findViewById(R.id.route);
+        reGenQr = (TextView) findViewById(R.id.reGenQr);
+        imgRoute = (ImageButton) findViewById(R.id.imgRoute);
 
+        des = (TextView) findViewById(R.id.des);
+        webPage = (TextView) findViewById(R.id.webPage);
+
+        placeTypeBar = (TextView) findViewById(R.id.placeType_bar);
+        placeNameBar = (TextView) findViewById(R.id.placeName_bar);
 
         Intent i = getIntent();
         sQr = i.getStringArrayExtra("sQr");
@@ -67,7 +72,28 @@ public class ResultReadQrActivity  extends AppCompatActivity {
         addDatabaseQr.addDb();
         showData();
 
-        route.setOnClickListener(new View.OnClickListener() {
+        webPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse(webPage.getText().toString());
+
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
+//        route.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                //ist<Float> latLng = Arrays.asList(dateQr.getLat(), dateQr.getLng());
+//                Intent i = new Intent(getApplicationContext(), RouteMap.class);
+//                i.putExtra("lat", dateQr.getLat());
+//                i.putExtra("lng", dateQr.getLng());
+//                startActivity(i);
+//            }
+//        });
+
+        imgRoute.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //ist<Float> latLng = Arrays.asList(dateQr.getLat(), dateQr.getLng());
@@ -135,12 +161,14 @@ public class ResultReadQrActivity  extends AppCompatActivity {
     }
 
     private void showData(){
-        titles.setText(dateQr.getTitles());
-        placeName.setText(dateQr.getPlaceName());
-        placeType.setText(dateQr.getPlaceType());
+//        titles.setText(dateQr.getTitles());
+//        placeName.setText(dateQr.getPlaceName());
+//        placeType.setText(dateQr.getPlaceType());
         des.setText(dateQr.getDes());
-        latLng.setText(dateQr.getLat()+", "+dateQr.getLng());
+//        latLng.setText(dateQr.getLat()+", "+dateQr.getLng());
         webPage.setText(dateQr.getWebPage());
+        placeNameBar.setText(dateQr.getPlaceName());
+        placeTypeBar.setText(dateQr.getPlaceType());
     }
 
 
